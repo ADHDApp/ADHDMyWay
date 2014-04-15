35b249aebb530cf8234445ef48599b7a9581bae3
nhess <hessn32@uwosh.edu>
2014-04-14 22:29:23 -0500

    Sign up page 1

---
+++ ADHDMyWay/src/com/example/adhdmyway/ProfileSetupActivity.java
@@ -0,0 +1,22 @@
+package com.example.adhdmyway;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ProfileSetupActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_profile_setup);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.profile_setup, menu);
+		return true;
+	}
+
+}
